890aa678b93a91b10101e505cb30bcec01e3c53f
Shrey vats <techtechnicalshrey@gmail.com>
2025-07-04 12:07:29 +0530

    setup authroute

--- ai-post-creator/app.js
+++ ai-post-creator/app.js
@@ -1,12 +1,13 @@
 import e from "express";
 import cors from 'cors'
 import { configDotenv } from "dotenv";
+import authRoutes from "./routes/authRoutes";
 
 const app = e();
 
 app.use(cors())
 app.use(e.json())
 
-
+app.use("/api/", authRoutes)
 
 export default app
