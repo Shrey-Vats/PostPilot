7910a05b0b62c60219493967611b3eb28d3f01ba
Shrey vats <techtechnicalshrey@gmail.com>
2025-07-04 11:19:48 +0530

    setup mongoDB connection

---
+++ ai-post-creator/server.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import app from "./app.js";
+
+const PORT = process.env.PORT || 3000
+
+async function connectDB() {
+    try {
+      await mongoose.connect(process.env.MONGO_URL);
+      console.log("mongoDB connected ✅")
+
+      app.listen(PORT, ()=>{
+        console.log("🚀 Server Running on PORT", PORT)
+      })
+    } catch (error) {
+        console.log("mongoDB error 😈", error)
+    }
+}
+
+connectDB()
