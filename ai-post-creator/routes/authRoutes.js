1a8c721356f4e35366e7cb83206542700f3c91c2
Shrey vats <techtechnicalshrey@gmail.com>
2025-07-04 12:07:42 +0530

    auth routes

--- ai-post-creator/routes/authRoutes.js
+++ ai-post-creator/routes/authRoutes.js
@@ -1,1 +1,9 @@
 import e from 'express'
+import { Login, Signup } from '../controllers/authController.js'
+
+const authRoutes = e.Router()
+
+authRoutes.post("/signup", Signup)
+authRoutes.post("/signup", Login)
+
+export default authRoutes
