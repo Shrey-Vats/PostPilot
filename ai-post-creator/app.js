1300e3c6fbc60f63fffd56bcd1b89078676af4f7
Shrey vats <techtechnicalshrey@gmail.com>
2025-07-04 15:56:05 +0530

    add cookies setup

--- ai-post-creator/app.js
+++ ai-post-creator/app.js
@@ -2,11 +2,13 @@ import e from "express";
 import cors from 'cors'
 import { configDotenv } from "dotenv";
 import authRoutes from "./routes/authRoutes";
+import cookieParser from "cookie-parser";
 
 const app = e();
 
 app.use(cors())
 app.use(e.json())
+app.use(cookieParser());
 
 app.use("/api/", authRoutes)
 
