73ab7e839e27fe912b73a82c2962e6ec4a39030a
Shrey vats <techtechnicalshrey@gmail.com>
2025-07-11 00:06:52 +0530

    update: app.js as per authjs

--- ai-post-creator/app.js
+++ ai-post-creator/app.js
@@ -1,15 +1,18 @@
+import { ExpressAuth } from "@auth/express";
 import e from "express";
-import cors from 'cors'
+import cors from "cors";
 import { configDotenv } from "dotenv";
 import authRoutes from "./routes/userRoutes";
 import cookieParser from "cookie-parser";
 
 const app = e();
 
-app.use(cors())
-app.use(e.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(e.json());
 app.use(cookieParser());
 
-app.use("/api/users", authRoutes)
+app.use("/auth/*", ExpressAuth({ providers: [] }))
+app.use("/auth", authRoutes);
 
-export default app
+export default app;
