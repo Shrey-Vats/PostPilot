50a27948e1de06a6f4559235fddbacf0d2336eff
Shrey vats <techtechnicalshrey@gmail.com>
2025-07-12 15:57:13 +0530

    update: app.js for postr=Route and Inngest

--- ai-post-creator/app.js
+++ ai-post-creator/app.js
@@ -1,9 +1,15 @@
 import { ExpressAuth } from "@auth/express";
 import e from "express";
 import cors from "cors";
-import { configDotenv } from "dotenv";
-import authRoutes from "./routes/userRoutes";
 import cookieParser from "cookie-parser";
+import {serve} from "inngest/express"
+import { configDotenv } from "dotenv";
+
+import authRoutes from "./routes/userRoutes.js";
+import postRouter from "./routes/postRoutes.js";
+import { inngest } from "./inngest/client.js";
+import { onUserSignupComplete, onUserSigningUp } from "./inngest/functions/on-signup.js";
+import { onPostCreated } from "./inngest/functions/on-post-create.js";
 
 const app = e();
 
@@ -11,8 +17,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(e.json());
 app.use(cookieParser());
-
 app.use("/auth/*", ExpressAuth({ providers: [] }))
-app.use("/auth", authRoutes);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRouter)
+
+app.use("/api/inngest", serve({
+    client: inngest,
+    functions: [onUserSigningUp, onUserSignupComplete, onPostCreated]
+})
+)
 
 export default app;
